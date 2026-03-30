f8caf4e5b6b479176f0b4acc59e2d2740620e803
Benjamin Kraatz <benjaminkraatz@yahoo.com>
2026-03-30 09:20:42 +0000

    Update Findpoppler.cmake to require poppler version 22.01.0 and adjust include paths; change dev.sh permissions to executable

--- cmake/modules/Findpoppler.cmake
+++ cmake/modules/Findpoppler.cmake
@@ -1,22 +1,20 @@
 #include(FindPkgConfig)
 find_package(PkgConfig QUIET)
-pkg_search_module(poppler libpoppler>=24.02.0 poppler>=24.02.0)
+pkg_search_module(poppler libpoppler>=22.01.0 poppler>=22.01.0)
 if (poppler_FOUND)
-	pkg_search_module(poppler_cpp REQUIRED libpoppler-cpp>=24.02.0 poppler-cpp>=24.02.0)
+	pkg_search_module(poppler_cpp REQUIRED libpoppler-cpp>=22.01.0 poppler-cpp>=22.01.0)
 endif(poppler_FOUND)
  
 find_path(poppler_INCLUDE_DIR
-	NAMES poppler-config.h
-	PATHS ${PKG_poppler_INCLUDE_DIRS} ${poppler_INCLUDE_DIRS} /usr/local/include /usr/include
-	PATH_SUFFIXES poppler
-	NO_DEFAULT_PATH
+	NAMES poppler/ErrorCodes.h poppler/poppler-config.h
+	HINTS ${PKG_poppler_INCLUDE_DIRS} ${poppler_INCLUDE_DIRS}
+	PATHS /usr/local/include /usr/include
 	)
 
 find_path(poppler_cpp_INCLUDE_DIR
-	NAMES poppler-version.h
-	PATHS ${PKG_poppler_cpp_INCLUDE_DIRS} ${poppler_cpp_INCLUDE_DIRS} /usr/local/include /usr/include
-	PATH_SUFFIXES poppler
-	NO_DEFAULT_PATH
+	NAMES poppler/cpp/poppler-version.h
+	HINTS ${PKG_poppler_cpp_INCLUDE_DIRS} ${poppler_cpp_INCLUDE_DIRS}
+	PATHS /usr/local/include /usr/include
 	)
 
 find_library(poppler_LIBRARY
